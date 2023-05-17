5d4d8479a98fe1857dfb36d503409400d13e9b7e
ninekeem <ninekeem@cock.li>
2023-05-17 23:14:50 +0700

    configs added

--- nvim/lua/plugins/other.lua
+++ nvim/lua/plugins/other.lua
@@ -1,9 +1,10 @@
 return {
-		"neovim/nvim-lspconfig",
+        "cuducos/yaml.nvim"
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-cmdline",
 		"hrsh7th/nvim-cmp",
-		"L3MON4D3/LuaSnip"
+		"L3MON4D3/LuaSnip",
+		"neovim/nvim-lspconfig"
 }
