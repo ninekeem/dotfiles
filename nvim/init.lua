4e1ea0f08311fce505843c093a9701b20c190cc3
ninekeem <ninekeem@cock.li>
2023-11-29 01:25:38 +0700

    some stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -23,14 +23,28 @@ vim.opt.clipboard = "unnamedplus"
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 
 require("lazy").setup({
-  "folke/which-key.nvim",
-  { "folke/neoconf.nvim", cmd = "Neoconf" },
-  "folke/neodev.nvim",
-  "davidhalter/jedi-vim",
-  "bash-lsp/bash-language-server",
-  "lewis6991/gitsigns.nvim",
-  "terrortylor/nvim-comment",
-  "nvim-tree/nvim-tree.lua"
+	"folke/which-key.nvim",
+	{ "folke/neoconf.nvim", cmd = "Neoconf" },
+	"folke/neodev.nvim",
+	"davidhalter/jedi-vim",
+	"bash-lsp/bash-language-server",
+	"lewis6991/gitsigns.nvim",
+	"terrortylor/nvim-comment",
+	"nvim-tree/nvim-tree.lua",
+	{
+		"ray-x/go.nvim",
+		dependencies = {  -- optional packages
+			"ray-x/guihua.lua",
+	    	"neovim/nvim-lspconfig",
+	    	"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+	    	require("go").setup()
+		end,
+		event = {"CmdlineEnter"},
+		ft = {"go", 'gomod'},
+		build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+	}
 })
 
 vim.api.nvim_create_autocmd('FileType', {
@@ -44,5 +58,9 @@ vim.api.nvim_create_autocmd('FileType', {
 })
 
 require('gitsigns').setup()
+require('go').setup()
 require('nvim_comment').setup()
 require('nvim-tree').setup()
+require('nvim-treesitter').setup()
+
+require('lspconfig').gopls.setup({})
